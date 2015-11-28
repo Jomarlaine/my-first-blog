c9e6dfe0ab69a0e451ac0454766c29fb0ca2de32
Jomarlaine <didier.marjolaine@gmail.com>
2015-11-28 17:20:16 +0100

    Modification html pour template dynamique

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
-    
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts':posts})
